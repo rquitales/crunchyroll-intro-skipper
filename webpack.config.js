1b7c7489b76dbc4873a4b718e5c9c1114cf4d56d
Ramon Quitales <quitales.ramon@gmail.com>
2021-03-01 11:13:51 +1300

    Add Typescript compiler options
    This commit initialises the setup to convert this Chrome extension to be
    Typescript based.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { optimize } = require('webpack');
+const { join } = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: {
+    background_script: join(__dirname, 'src/background_script.ts'),
+    content_script: join(__dirname, 'src/content_script.ts'),
+  },
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+};
